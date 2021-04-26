a110452dcdcb89d4c17769ce73b896f3afb0a757
LadyMarina <49723271+LadyMarina@users.noreply.github.com>
2021-04-26 20:10:13 +0200

    Update

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -82,7 +82,7 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
-           // print("Xd");
+           
             if (!headBob.Resetted)
             {
                 headBob.ResetHeadBob();
