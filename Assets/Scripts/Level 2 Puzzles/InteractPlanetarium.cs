d0aa7d22006e5e3a2bf9ac6343a5baa4c071c563
LadyMarina <49723271+LadyMarina@users.noreply.github.com>
2021-04-29 22:58:36 +0200

    FinishPuzzle

--- Assets/Scripts/Level 2 Puzzles/InteractPlanetarium.cs
+++ Assets/Scripts/Level 2 Puzzles/InteractPlanetarium.cs
@@ -101,9 +101,9 @@ public class InteractPlanetarium : MonoBehaviour, IInteractable
 
         Win();
 
-        print((ringZero.localRotation.eulerAngles.z % 360 == 0)+ "Local Euler: " + ringZero.localEulerAngles.z + " Rotation: "+ ringZero.eulerAngles.z);
-        print((ringOne.localRotation.eulerAngles.z % 360 == 0) + "Local Euler: " + ringOne.localEulerAngles.z + " Rotation: " + ringOne.eulerAngles.z);
-        print((ringTwo.localRotation.eulerAngles.z % 360 == 0) + "Local Euler: " + ringTwo.localEulerAngles.z + " Rotation: " + ringTwo.eulerAngles.z);
+        print((ringZero.transform.localRotation.eulerAngles.z % 360 == 0)+ "Local Euler: " + ringTwo.transform.localRotation.eulerAngles.z);
+        print((ringOne.transform.localRotation.eulerAngles.z % 360 == 0) + "Local Euler: " + ringTwo.transform.localRotation.eulerAngles.z);
+        print((ringTwo.transform.localRotation.eulerAngles.z % 360 == 0) + "Local Euler: " + ringTwo.transform.localRotation.eulerAngles.z);
     }
 
     private void Win()
