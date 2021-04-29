55efebcd69c9ad74e3d7b8e2ee2504cd2740a26a
LadyMarina <49723271+LadyMarina@users.noreply.github.com>
2021-04-29 21:48:50 +0200

    TextPlanetarium

--- Assets/Scripts/Character/InteractSystem/PlaceMaterial.cs
+++ Assets/Scripts/Character/InteractSystem/PlaceMaterial.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlaceMaterial : MonoBehaviour
 {
@@ -9,6 +10,11 @@ public class PlaceMaterial : MonoBehaviour
     public int materialNumber;
     public bool correctMaterial;
 
+    public GameObject textBox;
+    public string text;
+    public float timeDuration;
+    private bool textDone = false;
+    private float time;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +24,31 @@ public class PlaceMaterial : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        time += Time.deltaTime;
+
         if (hasBeenPlaced && correctMaterial == true)
         {
             puzzle.GetComponent<InteractPlanetarium>().allClues[materialNumber - 1] = true;
+
+            if (textDone == false)
+            {
+                textDone = true;
+                time = 0;
+            }
+
+           
+
+            if (time > timeDuration)
+            {
+ 
+                textBox.gameObject.transform.parent.gameObject.SetActive(false);
+            }
+            else
+            {
+                textBox.GetComponent<Text>().text = text;
+                textBox.gameObject.transform.parent.gameObject.SetActive(true);
+                
+            }
         }
 
         if (hasBeenPlaced ==false)
---
+++ Assets/Scripts/Level 2 Puzzles/PlanetariumText.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlanetariumText : MonoBehaviour
+{
+
+    public string jupiter;
+    public string saturno;
+    public string marte;
+
+   
+
+    private bool jupiterIn = false;
+    private bool saturnoIn = false;
+    private bool marteIn = false;
+
+    private float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
