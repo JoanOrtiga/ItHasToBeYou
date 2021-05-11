6f2f7c86e05dfcf4e3dc30758074404097a39159
JoanStark <joanorba@gmail.com>
2021-05-11 13:47:33 +0200

    Ordering puzzles, added puzzle 1.1

---
+++ Assets/Scripts/Puzzle1/RotateStatues.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateStatues : MonoBehaviour , IInteractable
+{
+    public enum StatueSide
+    {
+        Front_Left, Front, Front_Right, Right, Back_Right, Back, Back_Left, Left
+    }
+
+    public StatueSide currentState;
+    public StatueSide objectiveState;
+
+    [SerializeField] private float rotationSpeed;
+
+    private bool rotating = false;
+
+    private Vector3 finalRotation;
+
+    public void Interact()
+    {
+        if(rotating)
+            return;
+        
+        rotating = true;
+
+        finalRotation = transform.rotation.eulerAngles;
+
+        finalRotation.y = finalRotation.y + 45;
+    }
+
+    private void Update()
+    {
+        if (rotating)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(finalRotation), rotationSpeed * Time.deltaTime);
+
+            if (transform.rotation == Quaternion.Euler(finalRotation))
+            {
+                rotating = false;
+                
+                UpdateStatueSide();
+            }
+        }
+    }
+
+    public bool Solved()
+    {
+        return currentState == objectiveState;
+    }
+
+    private void UpdateStatueSide()
+    {
+        if ((int) currentState == 7)
+        {
+            currentState = StatueSide.Front_Left;
+        }
+        else
+            currentState = (StatueSide) ((int) currentState + 1);
+    }
+}
