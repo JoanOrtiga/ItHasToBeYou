c35837512c9fb9c45936f5797b3b1ea8bb12b08f
JoanStark <joanorba@gmail.com>
2021-06-17 19:39:14 +0200

    Prefab 3

--- Assets/ChangeEmissive.cs
+++ Assets/ChangeEmissive.cs
@@ -6,14 +6,38 @@ using UnityEngine;
 public class ChangeEmissive : MonoBehaviour
 {
     public Material material;
+    public Transform referencePoint;
+
+    private PlayerController playerController;
+
+    private bool onRange = false;
+
+    public AnimationCurve distance;
+
+    private void Awake()
+    {
+        playerController = FindObjectOfType<PlayerController>();
+    }
+
+    private void Update()
+    {
+        if(!onRange)
+            return;
+
+        material.SetColor("_EmissionColor", Color.white * distance.Evaluate((playerController.transform.position - referencePoint.position).magnitude));
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        material.SetColor("_EmissionColor", Color.white * 2f);
+        if (other.CompareTag("Player"))
+            onRange = true;
+        //material.SetColor("_EmissionColor", Color.white * 2f);
     }
 
     private void OnTriggerExit(Collider other)
     {
-        material.SetColor("_EmissionColor", Color.black);
+        if (other.CompareTag("Player"))
+            onRange = false;
+        //material.SetColor("_EmissionColor", Color.black);
     }
 }
