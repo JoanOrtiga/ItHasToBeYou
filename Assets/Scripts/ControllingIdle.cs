007fc56dbb7d50a3b83b69fed563e4ef8d60198a
LadyMarina <49723271+LadyMarina@users.noreply.github.com>
2021-06-17 21:56:23 +0200

    Revert "Merge branch 'main' into Puzzle2.1"
    This reverts commit 2a81001c5345aae36f41db1affeb996b4937be39, reversing
    changes made to cfbb033f46cdc14b21849d5cbe73112db705047e.

--- Assets/Scripts/ControllingIdle.cs
+++ Assets/Scripts/ControllingIdle.cs
@@ -10,10 +10,11 @@ public class ControllingIdle : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (render == null)
-        {
+        
             render = animator.GetComponentsInChildren<Renderer>();
-        }
+        
+
+        Debug.Log(render.Length);
         
         foreach (var renderer in render)
         {
@@ -32,10 +33,9 @@ public class ControllingIdle : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (render == null)
-        {
+        
             render = animator.GetComponentsInChildren<Renderer>();
-        }
+        
 
         foreach (var renderer in render)
         {
